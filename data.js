c0fcd771f5f595ad4222ccd62c508ff54117ef42
August Würtz Østergaard <augustwoec@gmail.com>
2025-01-04 17:40:15 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Catedral_G%C3%B3tica_de_Le%C3%B3n_edited.jpg",
+            alt: "Leon Cathedral"
+        },
+        title: "Leon",
+        country: "Spain",
+        googleMapsLink: "https://www.google.com/maps/place/León,+Spain",
+        dates: "1 Apr, 2022 - 2 Apr, 2022",
+        text: "Leon is the starting point of my Camino de Santiago journey. It's known for its impressive cathedral and rich history."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Astorga_Cathedral_Fa%C3%A7ade.jpg/1200px-Astorga_Cathedral_Fa%C3%A7ade.jpg",
+            alt: "Astorga Bishop's Palace"
+        },
+        title: "Astorga",
+        country: "Spain",
+        googleMapsLink: "https://www.google.com/maps/place/Astorga,+Spain",
+        dates: "3 Apr, 2022 - 4 Apr, 2022",
+        text: "Astorga is famous for its beautiful bishop's palace and its role in the Camino pilgrimage route."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Castillo_templario_de_Ponferrada_001.jpg/220px-Castillo_templario_de_Ponferrada_001.jpg",
+            alt: "Ponferrada Castle"
+        },
+        title: "Ponferrada",
+        country: "Spain",
+        googleMapsLink: "https://www.google.com/maps/place/Ponferrada,+Spain",
+        dates: "5 Apr, 2022 - 6 Apr, 2022",
+        text: "Ponferrada is known for its impressive castle and historic significance along the Camino route."
+    },
+    {
+        id: 4,
+        img: {
+            src: "https://www.eatlivetravelwrite.com/wp-content/uploads/2019/08/Snapseed-478.jpg",
+            alt: "Triacastella View"
+        },
+        title: "Triacastella",
+        country: "Spain",
+        googleMapsLink: "https://www.google.com/maps/place/Triacastela,+Spain",
+        dates: "7 Apr, 2022 - 8 Apr, 2022",
+        text: "Triacastella is a small village nestled in the Galicia region, an important stop for pilgrims as they approach the end of their journey."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://whereisgrandma.com/wp-content/uploads/2015/10/img_7918_pe.jpg?w=1200",
+            alt: "Sarria Monastery"
+        },
+        title: "Sarria",
+        country: "Spain",
+        googleMapsLink: "https://www.google.com/maps/place/Sarria,+Spain",
+        dates: "9 Apr, 2022 - 10 Apr, 2022",
+        text: "Sarria is one of the most popular starting points for pilgrims, and it offers charming streets and historical sites."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSadUAaOEqVi5E_fDb3w3NTHJzN_0AsJ412Gg&s",
+            alt: "Santiago de Compostela Cathedral"
+        },
+        title: "Santiago de Compostela",
+        country: "Spain",
+        googleMapsLink: "https://www.google.com/maps/place/Santiago+de+Compostela,+Spain",
+        dates: "11 Apr, 2022 - 14 Apr, 2022",
+        text: "Santiago de Compostela is the final destination of my Camino de Santiago journey, renowned for its cathedral."
+    },
+]
